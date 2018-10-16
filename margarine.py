2e0782466581375509a675e6372570d0d1e3d66b
APA092 <alexey.parilov@gmail.com>
2018-10-16 10:54:18 +0000

    variables created

--- margarine.py
+++ margarine.py
@@ -1,36 +1,27 @@
 from ortools.linear_solver import pywraplp
 
 def main():
-    data = [['January', 110, 120, 130, 110, 115],
-            ['February', 130, 130, 110, 90, 115],
-            ['March', 110, 140, 130, 100, 95],
-            ['April', 120, 110, 120, 120, 125],
-            ['May', 100, 120, 150, 110, 105],
-            ['June', 90, 100, 140, 80, 135]
+    data = [[110, 120, 130, 110, 115],
+            [130, 130, 110, 90, 115],
+            [110, 140, 130, 100, 95],
+            [120, 110, 120, 120, 125],
+            [100, 120, 150, 110, 105],
+            [90, 100, 140, 80, 135]
         ];
         
-    name = ['veg1', 'veg2', 'veg3', 'animal1', 'animal2'];
-    solver = pywraplp.Solver('ProductPlanning', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-    
-    x = [[]]*len(data)
-    
-    for i in range(0, len(data[0])-1):
-        x[i] = solver.NumVar(0, solver.infinity(), name[i])
+    solver = pywraplp.Solver('Linear_test', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
         
-    constraint = solver.Constraint(0,250)
-    for i in range(0,len(data[0])-1):
-        constraint.SetCoefficient(x[i], data[0][i+1])
-
-    objective = solver.Objective()
-    for i in range(0,len(data[0])-1):
-        objective.SetCoefficient(x[i], 1)
-    objective.SetMaximization()
-    
-    solver.Solve()
-    for i in range(0, 4):
-        print x[i]
-        print x[i].solution_value()
-    
+  #create variables
+    buy = [[0 for x in range(len(data[0]))] for y in range(len(data))] 
+    produce = [[0 for x in range(len(data[0]))] for y in range(len(data))] 
+    store = [[0 for x in range(len(data[0]))] for y in range(len(data))] 
+  
+    for i in range(0, len(data)):
+        for j in range(0, len(data[0])):
+            buy[i][j] = solver.NumVar(0, solver.infinity(), 'buy')
+            produce[i][j] = solver.NumVar(0, solver.infinity(), 'produce')
+            store[i][j] = solver.NumVar(0, solver.infinity(), 'store')
+    print len(buy)
     
 if __name__ == '__main__':
   main()
