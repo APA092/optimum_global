33315c8cd868201949b4eaee0749df8eaf3e55f4
APA092 <alexey.parilov@gmail.com>
2018-10-17 12:15:08 +0000

    all constraints added except for product quality

--- margarine.py
+++ margarine.py
@@ -19,7 +19,7 @@ def main():
     for i in range(0, len(data)):
         for j in range(0, len(data[0])):
             buy[i][j] = solver.NumVar(0, solver.infinity(), 'buy')
-            produce[i][j] = solver.NumVar(0, solver.infinity(), 'produce')
+            produce[i][j] = solver.NumVar(0, 600, 'produce')
             store[i][j] = solver.NumVar(0, solver.infinity(), 'store')
    
 #create objective
@@ -34,7 +34,6 @@ def main():
 #create constraints
     #production not higher than capacity of machine 1
     constraint1 = [0]*len(produce)
-    
     for i in range(0, 6):
         constraint1[i] = solver.Constraint(0, 250)
         for j in range(0, 3):
@@ -44,8 +43,50 @@ def main():
     constraint2 = [0]*len(produce)
     for i in range(0, 6):
         constraint2[i] = solver.Constraint(0, 200)
-        for j in range(4, 5):
+        for j in range(3, 5):
             constraint2[i].SetCoefficient(produce[i][j],1)
+            
+    #production not higher than resources available
+    constraint3 = [[0 for x in range(len(data[0]))] for y in range(len(data))]
+    for i in range(0, len(produce)):
+        for j in range(0, len(produce[0])):
+            constraint3[i][j] = solver.Constraint(0, solver.infinity())
+            constraint3[i][j].SetCoefficient(produce[i][j], -1)
+            constraint3[i][j].SetCoefficient(store[i][j], 1)
+            constraint3[i][j].SetCoefficient(buy[i][j], 1)
+            
+    #storage limited to 500 units
+    constraint4 = [[0 for x in range(len(data[0]))] for y in range(len(data))]        
+    for i in range(0, len(produce)):
+        for j in range(0, len(produce[0])):    
+            constraint4[i][j] = solver.Constraint(0, 500)
+            constraint4[i][j].SetCoefficient(store[i][j], 1)
+            
+    #initial storage
+    constraint5 = [0]*len(store[0])
+    for i in range(0, len(store[0])):
+        constraint5[i] = solver.Constraint(500, 500)
+        constraint5[i].SetCoefficient(store[0][i],1)
+        constraint5[i].SetCoefficient(buy[0][i],-1)
+        constraint5[i].SetCoefficient(produce[0][i],1)
+    
+    #final storage
+    constraint6 = [0]*len(store[0])
+    for i in range(0, len(store[0])):
+        constraint6[i] = solver.Constraint(500, 500)
+        constraint6[i].SetCoefficient(store[4][i],1)
+        constraint6[i].SetCoefficient(buy[5][i],1)
+        constraint6[i].SetCoefficient(produce[5][i],-1)
+    
+    #linking storage and production
+    constraint7 = [[0 for x in range(len(data[0]))] for y in range(len(data))]
+    for i in range(1,6):
+        for j in range(0,len(data[0])):
+            constraint7[i][j] = solver.Constraint(0, 0)
+            constraint7[i][j].SetCoefficient(store[i-1][j],1)
+            constraint7[i][j].SetCoefficient(store[i][j],-1)
+            constraint7[i][j].SetCoefficient(buy[i][j],1)
+            constraint7[i][j].SetCoefficient(produce[i][j],-1)
             
     solver.Solve()
     
@@ -58,6 +99,26 @@ def main():
         str(produce[i][2].solution_value()) + ' '+  \
         str(produce[i][3].solution_value()) + ' ' +  \
         str(produce[i][4].solution_value())
+        
+    print '***************'
+    print 'Storage Plan'
+    print '***************'
+    for i in range(0, len(buy)):
+        print str(store[i][0].solution_value()) + ' '\
+        +  str(store[i][1].solution_value()) + ' '+  \
+        str(store[i][2].solution_value()) + ' '+  \
+        str(store[i][3].solution_value()) + ' ' +  \
+        str(store[i][4].solution_value())
+        
+    print '***************'
+    print 'Purchase Plan'
+    print '***************'
+    for i in range(0, len(buy)):
+        print str(buy[i][0].solution_value()) + ' '\
+        +  str(buy[i][1].solution_value()) + ' '+  \
+        str(buy[i][2].solution_value()) + ' '+  \
+        str(buy[i][3].solution_value()) + ' ' +  \
+        str(buy[i][4].solution_value())
         
         
 if __name__ == '__main__':
